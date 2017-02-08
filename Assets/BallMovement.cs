276738f0bbda1c7e4f8a48d4c9594845dea9276a
Unknown <kelseyhu2425@gmail.com>
2017-02-08 18:09:13 -0500

    projectile motion problem fixed

---
+++ Assets/BallMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : MonoBehaviour {
+
+	public float xSpeed = 0.1f;
+	public float ySpeed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D>().velocity = new Vector3(xSpeed, ySpeed, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+}
