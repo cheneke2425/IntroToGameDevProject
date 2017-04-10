eb862487b94b09f38b6e3372160b00867b9c28cc
Unknown <kelseyhu2425@gmail.com>
2017-04-09 23:03:37 -0400

    Character moving animation

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -11,6 +11,11 @@ public class BallScript : MonoBehaviour {
 
 	public GameObject YouWin;
 
+	private Animator animator;
+
+	private float xValue;
+	private float yValue;
+
 	// Use this for initialization
 	void Start () {
 
@@ -31,27 +36,64 @@ public class BallScript : MonoBehaviour {
 
 			if (hit && hit.transform.gameObject.CompareTag("Replay"))
 			{
-				Start();
-				GetComponent<Rigidbody2D>().velocity = new Vector3(xSpeed, ySpeed, 0);
+				animator = hit.transform.gameObject.GetComponent<Animator>();
+				animator.SetTrigger("OnClick");
+				StartCoroutine(WaitForMovement());
 			} 
 		}
-	
+
+		xValue = GetComponent<Rigidbody2D>().velocity.x;
+		yValue = GetComponent<Rigidbody2D>().velocity.y;
+
+		if (GetComponent<Rigidbody2D>().velocity != new Vector2(0,0))
+		{
+			float angle = Mathf.Atan2(yValue, xValue) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis(angle+90, Vector3.forward);
+		}
+
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
-
 		if (other.gameObject.CompareTag("Goal"))
 		{
-			GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
-			Debug.Log("YOU WIN!");
-			YouWin.transform.position = new Vector3(0, 0, 0);
+			Win();
 		}
 		else if (other.gameObject.CompareTag("Obstacle"))
 		{
-			GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
-			Debug.Log("YOU LOSE!");
+			Lose();
 		}
+	}
+
+	void Win()
+	{
+		GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
+		Debug.Log("YOU WIN!");
+		YouWin.transform.position = new Vector3(0, 0, 0);
+	}
+
+	void Lose()
+	{
+		GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
+		Debug.Log("YOU LOSE!");
+		StartCoroutine(WaitForRestart());
+	}
+
+	void Movement()
+	{
+		GetComponent<Rigidbody2D>().velocity = new Vector3(xSpeed, ySpeed, 0);
+	}
+
+	IEnumerator WaitForMovement()
+	{
+		yield return new WaitForSeconds(0.833f);
+		Movement();
+	}
+
+	IEnumerator WaitForRestart()
+	{
+		yield return new WaitForSeconds(3);
+		Application.LoadLevel ("Level#1");
 	}
 
 }
